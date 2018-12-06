167a330b06b722c87cb96928c6f7b98af5e3bfb4
Richard Ramos <info@richardramos.me>
2018-12-06 09:38:02 -0400

    Long press to drag

--- app/components/flow/create/PollOptions.js
+++ app/components/flow/create/PollOptions.js
@@ -123,7 +123,7 @@ class PollOptions extends Component {
             <Typography variant="headline">Create a Poll</Typography>
             <Typography variant="body1" style={{marginTop: '20px'}}>Add options to the poll</Typography>
             <a onClick={this.handleClickOpen} className="addOption"><img src="images/plus-button.svg" width="40" />Add option</a>
-            <SortableList lockAxis={"y"} distance={10} items={this.state.options} editOption={this.editOption} removeOption={this.removeOption} onSortEnd={this.onSortEnd} />
+            <SortableList lockAxis={"y"} pressDelay={200} items={this.state.options} editOption={this.editOption} removeOption={this.removeOption} onSortEnd={this.onSortEnd} />
 
         </div>
         <div className="buttonNav">
